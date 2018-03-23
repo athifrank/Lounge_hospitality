334a73949fd8c44a3316f3d42caa24f850667183
Athinarayanan <athifrank@gmail.com>
2018-03-23 12:49:09 +0530

    grid box align

--- src/App.js
+++ src/App.js
@@ -2,55 +2,55 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      data:[]
+    this.state = {
+      data: []
     }
   }
-   
-  componentDidMount(){
-    const url='http://texpertise.in/data.php';
-    fetch(url).then(res=>res.json()).then((res)=>{this.setState({data:res})});
+
+  componentDidMount() {
+    const url = 'http://texpertise.in/data.php';
+    fetch(url).then(res => res.json()).then((res) => { this.setState({ data: res }) });
   }
 
-        
+
   render() {
-    const value=this.state.data;
+    const value = this.state.data;
     console.log(value);
     return (
       <div className="container">
-      <div className="row">
-      {this.state.data.map((val,key)=>{
-        return(
-          <div className="card p-3 col-12 col-md-6 col-lg-4" key={key}>
-              <div className="card-img">
-                  <img src={val.image}  style={{width:'100%',height:'220px'}} alt="food_items"  />
-              </div>
-              <div className="card-box">
-                  <h4 className="card-title mbr-fonts-style display-7" style={{fontFamily:'Arvo',fontWeight:'bold'}}>
-                      {val.name}
+        <div className="row">
+          {this.state.data.map((val, key) => {
+            return (
+              <div className="card p-3 col-12 col-md-6 col-lg-3" key={key}>
+                <div className="card-img">
+                  <img src={val.image} style={{ width: '100%', height: '186px' }} alt="food_items" />
+                </div>
+                <div className="card-box">
+                  <h4 className="card-title mbr-fonts-style display-7" style={{ fontFamily: 'Arvo', fontWeight: 'bold' }}>
+                    {val.name}
                   </h4>
-              </div>
-              
-              <div className="mbr-section-btn text-right">
-              <div style={{float:'left'}}>
-              { val.nonVeg=== true ? <div><span className="nonveg-dot"></span> Non-veg</div>:
-              <div><span className="veg-dot"></span> Veg</div>}               
-              </div>
-              {val.spicy===true ? <span className="redChilli"><img alt="red-chilli" src="http://icons.veryicon.com/png/Cartoon/Rave/Hot%20chili.png" className="chilli"/>
-              spicy</span>:""}
+                </div>
 
-                  <button onClick={(e)=>this.props.history.push(`/details/${key}`,val)} className="btn btn-primary display-4" id="viewDetails">
-                      view details
+                <div className="mbr-section-btn text-right">
+                  <div style={{ float: 'left' }}>
+                    {val.nonVeg === true ? <div><span className="nonveg-dot"></span> Non-veg</div> :
+                      <div><span className="veg-dot"></span> Veg</div>}
+                  </div>
+                  {val.spicy === true ? <span className="redChilli"><img alt="red-chilli" src="http://icons.veryicon.com/png/Cartoon/Rave/Hot%20chili.png" className="chilli" />
+                    spicy</span> : ""}
+
+                  <button onClick={(e) => this.props.history.push(`/details/${key}`, val)} className="btn btn-primary display-4" id="viewDetails">
+                    view details
                   </button>
+                </div>
               </div>
+            )
+          })}
+        </div>
       </div>
-        )
-      })}
-</div>
-</div>
-     
+
     )
   }
 }
--- src/Details.js
+++ src/Details.js
@@ -1,38 +1,38 @@
 import React from 'react';
 
-class Details extends React.Component{    
-    render(){
-const {location}=this.props;
+class Details extends React.Component {
+    render() {
+        const { location } = this.props;
 
-        return(
-           
-<div className="container">
-	<div className="row" style={{    marginTop: '78px'}}>
-                        <div className="col-md-4">
-                            <img className="lib-img-show" alt= {location.state.name} src={location.state.image} style={{width:'103%',height:'292px'}}/>
-                        </div>
-                        <div className="col-md-8">
-                            <div className="lib-row lib-header">
-                                {location.state.name}
-                                <div className="lib-header-seperator"></div>
-                            </div><br/>
-                            <div className="lib-row lib-desc">
+        return (
+
+            <div className="container">
+                <div className="row" style={{ marginTop: '78px',boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)' }}>
+                    <div className="col-md-4">
+                        <img className="lib-img-show" alt={location.state.name} src={location.state.image} style={{ width: '105%', height: '320px',margin: '10px 0px' }} />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="lib-row lib-header">
+                            {location.state.name}
+                            <div className="lib-header-seperator"></div>
+                        </div><br />
+                        <div className="lib-row lib-desc">
                             {location.state.description}
-                            </div>
+                        </div><br/><hr/>
 
 
 
-                        <div style={{float:'left'}}>
-              { location.state.nonVeg=== true ? <div><span className="nonveg-dot"></span> Non-veg</div>:
-              <div><span className="veg-dot"></span> Veg</div>}               
-              </div>
-              {location.state.spicy===true ? <span className="redChilli"><img alt="red-chilli" src="http://icons.veryicon.com/png/Cartoon/Rave/Hot%20chili.png" className="chilli"/>
-              spicy</span>:""}
-
-                  <button onClick={(e)=>this.props.history.goBack()} className="btn btn-success">GoBack</button>
+                        <div style={{ float: 'left' ,marginTop:'5px'}} >
+                            {location.state.nonVeg === true ? <div><span className="nonveg-dot"></span> Non-veg</div> :
+                                <div><span className="veg-dot"></span> Veg</div>}
                         </div>
+                        {location.state.spicy === true ? <span className="redChilli"><img alt="red-chilli" src="http://icons.veryicon.com/png/Cartoon/Rave/Hot%20chili.png" className="chilli" />
+                            spicy</span> : ""}
 
-            </div>
+                        <button onClick={(e) => this.props.history.goBack()} className="btn btn-success">GoBack</button>
+                    </div>
+
+                </div>
             </div>
         )
     }
